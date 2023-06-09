2db77380b6525e86b4727ca235dabe9b11104b93
Hamed Polo <hamedpolo@Hameds-Air-V2.home>
2023-06-09 00:30:17 -0400

    added constant variable and test cases for cash and miles conversion

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -2,13 +2,15 @@ public class RewardValue {
     double cashValue;
     int milesValue;
 
+    public static final double CONVERSION_RATE = 0.0035;
+
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = (int) (cashValue / 0.0035);
+        this.milesValue = (int) (cashValue / CONVERSION_RATE);
     }
     public RewardValue(int milesValue) {
         this.milesValue = milesValue;
-        this.cashValue = milesValue * 0.0035;
+        this.cashValue = milesValue * CONVERSION_RATE;
     }
 
     public double getCashValue() {
